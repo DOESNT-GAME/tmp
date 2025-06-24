cb587b9fddcc41fe8ee1a72abde110ce3125878a
chipap123123 <71125045+chipap123123@users.noreply.github.com>
2025-06-24 13:33:31 +0300

    Client Doxygen
    Документация для клиента

---
+++ client/html/search/classes_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['authdialog_0',['AuthDialog',['../class_auth_dialog.html',1,'']]],
+  ['authregcontainer_1',['AuthRegContainer',['../class_auth_reg_container.html',1,'']]]
+];
---
+++ client/html/search/searchdata.js
@@ -0,0 +1,18 @@
+var indexSectionsWithContent =
+{
+  0: "amrs",
+  1: "amrs"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes"
+};
+
