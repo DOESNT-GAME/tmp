cce625b32b71c5d078ec32cc20b8dcfb434b13eb
chipap123123 <71125045+chipap123123@users.noreply.github.com>
2025-06-24 13:34:18 +0300

    Server Doxygen
    Документация для сервера

---
+++ docker-server/html/files_dup.js
@@ -0,0 +1,6 @@
+var files_dup =
+[
+    [ "dbmanager.h", "dbmanager_8h_source.html", null ],
+    [ "mytcpserver.h", "mytcpserver_8h_source.html", null ],
+    [ "sha384.h", "sha384_8h_source.html", null ]
+];
---
+++ docker-server/html/hierarchy.js
@@ -0,0 +1,8 @@
+var hierarchy =
+[
+    [ "DatabaseManager", "class_database_manager.html", null ],
+    [ "QObject", null, [
+      [ "TcpServer", "class_tcp_server.html", null ]
+    ] ],
+    [ "SHA384", "class_s_h_a384.html", null ]
+];
